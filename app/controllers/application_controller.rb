7dcd8cb682b7992197e18fe41924d289a9abd382
yossu-y <yangyuub@gmail.com>
2022-04-23 14:56:50 +0000

    controllerから再開

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+
 
   def after_sign_in_path_for(resource)
     root_path
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "homes#top"
+  
   get "home/about"=>"homes#about", as: "about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
