70cac082452f232f3641d27108f02c094ee03993
yossu-y <yangyuub@gmail.com>
2022-04-24 03:36:06 +0000

    [update]controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  # before_action :ensure_correct_user, only: [:update]
 
   def show
     @user = User.find(params[:id])
@@ -13,13 +13,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(current_user), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
@@ -29,10 +31,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  def ensure_correct_user
-    @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to user_path(current_user)
-    end
-  end
+  # def ensure_correct_user
+    # @user = User.find(params[:id])
+    # unless @user == current_user
+      # redirect_to user_path(current_user)
+    # end
+  # end
 end
